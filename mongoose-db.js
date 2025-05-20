a8e492456875f8245f97b8b2127127833511890a
tachu1995 <dilapinc@gmail.com>
2025-05-21 00:36:03 +1000

    Update mongoose-db.js

--- mongoose-db.js
+++ mongoose-db.js
@@ -50,7 +50,7 @@ User.find({ username: "admin@snyk.io" }).exec(function (err, users) {
     console.log("no admin");
     new User({
       username: "admin@snyk.io",
-      password: "SuperSecretPassword123",
+      password: "NOPASSWORD",
     }).save(function (err, user, count) {
       if (err) {
         console.log("error saving admin user");
